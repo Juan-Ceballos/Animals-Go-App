0ed6eb673e107ff0a90544378668f23b3daa59b4
Juan-Ceballos <juanceballos@pursuit.org>
2025-01-29 22:58:43 -0500

    updated the create function and index switch case to write to file to update animals json

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const inform = console.log
 const {readJSONFile, writeJSONFile} = require('./src/helper')
 const animals = readJSONFile('./data', 'animals.json')
+const {create, index} = require('./src/animalController')
 
 function run() {
     const action = process.argv[2]
     const animal = process.argv[3]
+    let writeToFile = false
+    let updatedAnimals = []
+
     switch (action) {
         case 'index':
-            inform(action, animals)
+            const animalsView = index(animals)
+            inform(animalsView)
             break
         case 'create':
             inform(action, animal)
+            updatedAnimals = create(animals, animal)
+            writeToFile = true
             break
         case 'show':
             inform(action, animal)
@@ -27,5 +34,9 @@ function run() {
         default:
             inform('There was an error.')
 
+    }
+
+    if (writeToFile) {
+        writeJSONFile('./data', 'animals.json', updatedAnimals)
     }
 }
--- src/animalController.js
+++ src/animalController.js
@@ -1,12 +1,22 @@
 const {nanoid} = require('nanoid')
+const animalPoints = require('../data/animalPoints.json')
 
 function create(animals, animalName) {
-    const animal = {name: animalName, id: nanoid(4)}
+    const animal = {
+        name: animalName, 
+        id: nanoid(4),
+        points: animalPoints[animalName] || 10
+    }
     animals.push(animal)
     return animals
 }
 
+function index(animals) {
+    return animals.map((animal) => animal.id + ' ' + animal.name).join('\n')
+}
+
 module.exports = {
-    create
+    create,
+    index
 }
 
